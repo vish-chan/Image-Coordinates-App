d6d1b4e4cf5c577f7a6991aeea66b33b7936bd6c
Vishal <poison.iitr@gmail.com>
2020-03-07 01:13:30 +0530

    Updates

---
+++ precache-manifest.496763dfa07868ed851fa41d71b23e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15156a3e369896de42be6c2c14dd6d7",
+    "url": "/Image-Coordinates-App/index.html"
+  },
+  {
+    "revision": "27c27e0a0163c428e1a7",
+    "url": "/Image-Coordinates-App/static/css/main.3aeda0b6.chunk.css"
+  },
+  {
+    "revision": "3b59f633ff2f5059b020",
+    "url": "/Image-Coordinates-App/static/js/2.784d47ef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Image-Coordinates-App/static/js/2.784d47ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c27e0a0163c428e1a7",
+    "url": "/Image-Coordinates-App/static/js/main.047ef9d9.chunk.js"
+  },
+  {
+    "revision": "e57ce84237c934ac756d",
+    "url": "/Image-Coordinates-App/static/js/runtime-main.0e0b6f10.js"
+  }
+]);
